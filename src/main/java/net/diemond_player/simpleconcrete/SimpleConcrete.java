2ee7427262152e5a2822d1faca0c2ca9b1ce4d9d
Diemond_Player <diemondplay@gmail.com>
2024-10-02 18:47:42 +0500

    Initial commit

---
+++ src/main/java/net/diemond_player/simpleconcrete/SimpleConcrete.java
@@ -0,0 +1,17 @@
+package net.diemond_player.simpleconcrete;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SimpleConcrete implements ModInitializer {
+	public static final String MOD_ID = "simpleconcrete";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
