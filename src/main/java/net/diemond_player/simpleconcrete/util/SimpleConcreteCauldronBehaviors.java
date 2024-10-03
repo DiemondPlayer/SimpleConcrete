088e20da5ea6cbd4f883386c85691eaaa79b7913
Diemond_Player <diemondplay@gmail.com>
2024-10-03 18:11:57 +0500

    a better way to register behaviors (+ mod compat)

--- src/main/java/net/diemond_player/simpleconcrete/util/SimpleConcreteCauldronBehaviors.java
+++ src/main/java/net/diemond_player/simpleconcrete/util/SimpleConcreteCauldronBehaviors.java
@@ -8,6 +8,7 @@ import net.minecraft.block.ConcretePowderBlock;
 import net.minecraft.block.LeveledCauldronBlock;
 import net.minecraft.block.cauldron.CauldronBehavior;
 import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
@@ -16,32 +17,19 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.ItemActionResult;
 
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import static net.minecraft.block.cauldron.CauldronBehavior.WATER_CAULDRON_BEHAVIOR;
 
 public class SimpleConcreteCauldronBehaviors {
     public static void registerCauldronBehaviors(){
-
-        WATER_CAULDRON_BEHAVIOR.map().put(Items.WHITE_CONCRETE_POWDER, HARDEN_CONCRETE_POWDER);
-        WATER_CAULDRON_BEHAVIOR.map().put(Items.BLACK_CONCRETE_POWDER, HARDEN_CONCRETE_POWDER);
-        WATER_CAULDRON_BEHAVIOR.map().put(Items.BROWN_CONCRETE_POWDER, HARDEN_CONCRETE_POWDER);
-        WATER_CAULDRON_BEHAVIOR.map().put(Items.RED_CONCRETE_POWDER, HARDEN_CONCRETE_POWDER);
-        WATER_CAULDRON_BEHAVIOR.map().put(Items.BLUE_CONCRETE_POWDER, HARDEN_CONCRETE_POWDER);
-        WATER_CAULDRON_BEHAVIOR.map().put(Items.YELLOW_CONCRETE_POWDER, HARDEN_CONCRETE_POWDER);
-        WATER_CAULDRON_BEHAVIOR.map().put(Items.LIGHT_GRAY_CONCRETE_POWDER, HARDEN_CONCRETE_POWDER);
-        WATER_CAULDRON_BEHAVIOR.map().put(Items.GRAY_CONCRETE_POWDER, HARDEN_CONCRETE_POWDER);
-        WATER_CAULDRON_BEHAVIOR.map().put(Items.PINK_CONCRETE_POWDER, HARDEN_CONCRETE_POWDER);
-        WATER_CAULDRON_BEHAVIOR.map().put(Items.MAGENTA_CONCRETE_POWDER, HARDEN_CONCRETE_POWDER);
-        WATER_CAULDRON_BEHAVIOR.map().put(Items.PURPLE_CONCRETE_POWDER, HARDEN_CONCRETE_POWDER);
-        WATER_CAULDRON_BEHAVIOR.map().put(Items.LIGHT_BLUE_CONCRETE_POWDER, HARDEN_CONCRETE_POWDER);
-        WATER_CAULDRON_BEHAVIOR.map().put(Items.CYAN_CONCRETE_POWDER, HARDEN_CONCRETE_POWDER);
-        WATER_CAULDRON_BEHAVIOR.map().put(Items.ORANGE_CONCRETE_POWDER, HARDEN_CONCRETE_POWDER);
-        WATER_CAULDRON_BEHAVIOR.map().put(Items.GREEN_CONCRETE_POWDER, HARDEN_CONCRETE_POWDER);
-        WATER_CAULDRON_BEHAVIOR.map().put(Items.LIME_CONCRETE_POWDER, HARDEN_CONCRETE_POWDER);
-
-//        if(SimpleConcrete.DYE_DEPOT){
-//            WATER_CAULDRON_BEHAVIOR.map().put(Registries.BLOCK.stream().filter(
-//                    block -> Registries.BLOCK.getId(block).equals(Identifier.of(SimpleConcrete.DYE_DEPOT_MOD_ID, "mint_concrete_powder"))).findFirst().get().asItem(), HARDEN_CONCRETE_POWDER);
-//        }
+        List<Block> list = Registries.BLOCK.stream().filter(
+                block -> block instanceof ConcretePowderBlock).toList();
+        for(Block block : list){
+            WATER_CAULDRON_BEHAVIOR.map().put(block.asItem(), HARDEN_CONCRETE_POWDER);
+        }
     }
 
     public static final CauldronBehavior HARDEN_CONCRETE_POWDER = (state, world, pos, player, hand, stack) -> {
