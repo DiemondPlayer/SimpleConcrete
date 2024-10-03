32dc82bebb9a4f81430be7bc91d85de8d4e802d2
Diemond_Player <diemondplay@gmail.com>
2024-10-03 21:09:54 +0500

    minor adjustments

--- src/main/java/net/diemond_player/simpleconcrete/SimpleConcrete.java
+++ src/main/java/net/diemond_player/simpleconcrete/SimpleConcrete.java
@@ -3,16 +3,7 @@ package net.diemond_player.simpleconcrete;
 import net.diemond_player.simpleconcrete.util.SimpleConcreteCauldronBehaviors;
 import net.fabricmc.api.ModInitializer;
 
-import net.fabricmc.loader.api.FabricLoader;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class SimpleConcrete implements ModInitializer {
-	public static final String MOD_ID = "simpleconcrete";
-//	public static final String DYE_DEPOT_MOD_ID = "dye_depot";
-//	public static final boolean DYE_DEPOT = FabricLoader.getInstance().isModLoaded(DYE_DEPOT_MOD_ID);
-
-
 	@Override
 	public void onInitialize() {
 		SimpleConcreteCauldronBehaviors.registerCauldronBehaviors();
