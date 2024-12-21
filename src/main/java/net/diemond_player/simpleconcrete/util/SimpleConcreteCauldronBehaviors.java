ad6b13e03fee64c560111c70dee9ab92a0ca7dc2
Diemond_Player <diemondplay@gmail.com>
2024-12-21 12:34:44 +0500

    1.21.2 changes

--- src/main/java/net/diemond_player/simpleconcrete/util/SimpleConcreteCauldronBehaviors.java
+++ src/main/java/net/diemond_player/simpleconcrete/util/SimpleConcreteCauldronBehaviors.java
@@ -1,25 +1,16 @@
 package net.diemond_player.simpleconcrete.util;
 
-import net.diemond_player.simpleconcrete.SimpleConcrete;
-import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
 import net.minecraft.block.ConcretePowderBlock;
 import net.minecraft.block.LeveledCauldronBlock;
 import net.minecraft.block.cauldron.CauldronBehavior;
 import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.stat.Stats;
 import net.minecraft.util.ActionResult;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.ItemActionResult;
 
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 import static net.minecraft.block.cauldron.CauldronBehavior.WATER_CAULDRON_BEHAVIOR;
 
@@ -40,7 +31,7 @@ public class SimpleConcreteCauldronBehaviors {
             player.incrementStat(Stats.USE_CAULDRON);
             LeveledCauldronBlock.decrementFluidLevel(state, world, pos);
         }
-        return ItemActionResult.success(world.isClient);
+        return ActionResult.SUCCESS;
     };
 
 }
